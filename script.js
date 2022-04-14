fe663ed990f68031a4d608d9c4731c8b452fee4e
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-14 14:54:58 -0500

    added fade in, still needs fade out

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function toggleMenu(){
     const hiddenMenu = document.querySelector('#hidden-menu');
     const checkMenuClass = hiddenMenu.getAttribute('class');
+    const background = document.querySelector('.display');
+    const body = document.querySelector('body');
 
     if (checkMenuClass == 'menu-off'){
         hiddenMenu.setAttribute('class', 'menu-on');
+        background.setAttribute('style', 'opacity: 20%');
+        body.setAttribute('style', 'background-color: white');
     }
     else {
         hiddenMenu.setAttribute('class', 'menu-off');
+        background.setAttribute('style', 'opacity: 100%');
+        body.setAttribute('style', 'background-color: #dfb8c0');
     }
 }
